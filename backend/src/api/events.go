ab914877d96702514331c45009c249ff340ceec1
Lucas Serven <lserven@gmail.com>
2017-03-20 18:53:32 -0700

    backend/src/api: remove unused import and fix typo

--- backend/src/api/events.go
+++ backend/src/api/events.go
@@ -3,8 +3,6 @@ package api
 import (
 	"errors"
 	"time"
-
-	"gopkg.in/mgutz/dat.v1"
 )
 
 const (
@@ -143,7 +141,7 @@ func (api *API) triggerEventConsequences(instanceID, appID, groupID, lastUpdateV
 		return err
 	}
 
-	// TODO: should we also consider ResultSuccess in the next check? CoreOS ~ generic conflics?
+	// TODO: should we also consider ResultSuccess in the next check? CoreOS ~ generic conflicts?
 	if etype == EventUpdateComplete && result == ResultSuccessReboot {
 		_ = api.updateInstanceStatus(instanceID, appID, InstanceStatusComplete)
 
